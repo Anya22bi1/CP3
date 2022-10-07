a1f357df54daa8e4b48fe033d6eea261c2284b77
Anya22bi1 <agminaeva@edu.hse.ru>
2022-10-07 23:25:32 +0300

    Add file

---
+++ 1.py
@@ -0,0 +1,8 @@
+name = 'Anna'
+surname = 'Minaeva'
+dr = 2002
+print(name,'_',surname,'_',dr)
+name = 'Minaeva'
+surname = 'Anna'
+dr += 60
+print(name,'_',surname,'_',dr)
---
+++ 2.py
@@ -0,0 +1,6 @@
+storona_1 = 3
+storona_2 = 4
+storona_3 = (3**2 + 4**2)**(1/2)
+S = storona_1 * storona_2 /2
+P = storona_1 + storona_2 + storona_3
+print('Площадь:', S, 'Периметр:', P)
